beba8288d23f41e82072ec5c9bb6878e879bd85e
jeju <jeju@hpcnt.com>
2019-12-31 09:13:28 +0900

    fix swagger authentication

--- src/main/kotlin/com/ryulth/worklifebell/api/WorkLifeBell.kt
+++ src/main/kotlin/com/ryulth/worklifebell/api/WorkLifeBell.kt
@@ -2,8 +2,10 @@ package com.ryulth.worklifebell.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class WorkLifeBellApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/ryulth/worklifebell/api/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/ryulth/worklifebell/api/config/WebSecurityConfig.kt
@@ -3,8 +3,10 @@ package com.ryulth.worklifebell.api.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
@@ -20,23 +22,27 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
     val swaggerPassword: String = ""
 
     companion object {
-        val AUTH_LIST = arrayOf(
-            "/v2/api-docs",
-            "/configuration/ui",
-            "/swagger-resources",
-            "/configuration/security",
-            "/swagger-ui.html",
-            "/webjars/**"
+        val SWAGGER_LIST = arrayOf(
+            "/v2/api-docs" // for swagger
         )
+        const val SWAGGER = "SWAGGER"
     }
 
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.inMemoryAuthentication()
-            .withUser(swaggerId).password(passwordEncoder().encode(swaggerPassword)).roles("USER", "ADMIN")
+            .withUser(swaggerId).password(passwordEncoder().encode(swaggerPassword)).roles(SWAGGER)
     }
 
+    override fun configure(web: WebSecurity) {
+        web.ignoring().mvcMatchers(HttpMethod.OPTIONS, "/**")
+    }
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests().antMatchers(*AUTH_LIST).authenticated().and().csrf().disable()
+        http
+            .authorizeRequests()
+            .antMatchers(*SWAGGER_LIST).hasRole(SWAGGER)
+            .and()
+            .csrf().disable()
+            .httpBasic()
     }
 
     @Bean
