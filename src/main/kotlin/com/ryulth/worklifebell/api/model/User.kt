a987eb5d61bd932dce5456f9c0f46b4a439e2ee0
Ryulth <adboy94@khu.ac.kr>
2020-01-10 00:12:40 +0900

    Enumerated string

--- src/main/kotlin/com/ryulth/worklifebell/api/model/User.kt
+++ src/main/kotlin/com/ryulth/worklifebell/api/model/User.kt
@@ -1,11 +1,6 @@
 package com.ryulth.worklifebell.api.model
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "User")
@@ -15,5 +10,5 @@ data class User(
     val password: String,
     @Column(length = 16) val workLatitude: String? = null, // 위도
     @Column(length = 16) val workLongitude: String? = null, // 경도
-    val loginType: LoginType
+    @Enumerated(EnumType.STRING)val loginType: LoginType
 )
