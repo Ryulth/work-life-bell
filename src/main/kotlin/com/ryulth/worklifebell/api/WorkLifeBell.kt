af019c30e1526b503763e36b15b1624ab2ec11b0
jeju <jeju@hpcnt.com>
2020-01-10 23:38:31 +0900

    Add default timezone

--- src/main/kotlin/com/ryulth/worklifebell/api/WorkLifeBell.kt
+++ src/main/kotlin/com/ryulth/worklifebell/api/WorkLifeBell.kt
@@ -3,10 +3,17 @@ package com.ryulth.worklifebell.api
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableSwagger2
-class WorkLifeBellApplication
+class WorkLifeBellApplication {
+    @PostConstruct
+    fun started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<WorkLifeBellApplication>(*args)
